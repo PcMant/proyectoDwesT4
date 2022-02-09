3a1a4bb63f7b2cca788444885e609ffd533b5f48
Juan Molina <juan@pcmant.com>
2022-02-09 03:51:22 +0100

    update Phtml

---
+++ php/Phtml/Alertas.php
@@ -0,0 +1,79 @@
+<?php
+class Alertas{
+
+    public function error($mensaje){
+        return "
+        <div class='alert alert-danger alert-dismissible fade show' role='alert'>
+            <strong>Error!</strong> {$mensaje}
+            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+        </div>
+        ";
+    }
+
+    public function aviso($mensaje){
+        return "
+        <div class='alert alert-success alert-dismissible fade show' role='alert'>
+            <strong>Aviso!</strong> {$mensaje}
+            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+        </div>
+        ";
+    }
+}
+
+$alerta = new Alertas();
+
+// Comprobación de sesión y muestra una alerta de ser necesario
+if(
+    (empty($_SESSION['usuario']) || empty($_SESSION['password'])) &&
+    (!preg_match('/^login.php*/i',basename($_SERVER['REQUEST_URI'])) &&
+    !preg_match('/^register.php*/i',basename($_SERVER['REQUEST_URI']))) &&
+    !$_SESSION['bllogin']
+    )
+        $alerta->error('
+        Los usuarios invitados no pueden interacturar con la base de datos ni consultar datos, debe de iniciar sesión con un usuario registrado con suficientes privilegios.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <p>
+            Para poder hacer uso de esta plataforma no puede hacer uso de ella como Invitado, deberá de ser un usuario
+            registrado.
+        </p>
+        <p>
+            Existen dos tipos de cuentas:
+        </p>
+        <ul>
+            <li><strong>Visor</strong>: Este solo puede consultar los datos de los alumnos.</li>
+            <li><strong>Editor</strong>: Este puede Añadir datos de alumnos, modificarlos y consultar.</li>
+        </ul>
+        <a href="login.php" class="btn btn-primary mt-1">Iniciar sesión</a>
+        <a href="register.php" class="btn btn-primary mt-1">Registrarse</a>   
+        ');
+
+// Comprobación de privilegios mostrando una alerta si es necesario
+if((
+    preg_match('/^insert_alumnos.php*/i',basename($_SERVER['REQUEST_URI'])) ||
+    preg_match('/^update_alumnos.php*/i',basename($_SERVER['REQUEST_URI'])) ||
+    preg_match('/^delete_alumnos.php*/i',basename($_SERVER['REQUEST_URI']))
+    ) && $_SESSION['bllogin'] && !preg_match('/^Editor$/i',$_SESSION['tipo'])
+) 
+    $alerta->error('No dispones de suficientes privilegios para esta acción.');
+
+// Inicio de sesión
+if(isset($_GET['login']) && $_GET['login'] == 'true'){
+    $alerta->aviso('Has iniciado sesión correctamente.');
+}else if(isset($_GET['login']) && $_GET['login'] == 'false'){
+    $alerta->error('Contraseña o cuenta introduccida incorrectamente, intentalo de nuevo.');
+}
+
+// Formulario standar
+if(isset($_GET['formulario']) && $_GET['formulario'] == 'true'){
+    echo $alerta->aviso('Formulario procesado correctamente.');
+}else if(isset($_GET['formulario']) && $_GET['formulario'] == 'false'){
+    echo $alerta->error('Faltan datos o algunos de los datos introducciodos son incorrectos, introducelos nuevamente.');
+}
+
+// Aviso registro de usuario
+if(isset($_GET['registro']) && $_GET['registro'] == 'true'){
+    echo $alerta->aviso('El registro se ha realizado correctamente.');
+}else if(isset($_GET['registro']) && $_GET['registro'] == 'false'){
+    echo $alerta->error('Faltan datos o algunos de los datos introducciodos son incorrectos, introducelos nuevamente.');
+}
+?>
---
+++ php/Phtml/Phtml.php
@@ -0,0 +1,162 @@
+<?php 
+
+class Phtml{
+
+    protected $titulo;
+    protected $paginaUp;
+    protected $paginaDown;
+
+
+    function __construct($nuevoTitulo="PcMant"){
+        $this->titulo = $nuevoTitulo.'-NombreEscuela';
+        $this->pagina = '';
+    }
+
+    function __set($name,$value){
+        $this->$name = $value;
+    }
+
+    function __get($name){
+        return $this->$name;
+    }
+
+    function printPagina(){
+        // Inicio de sesión
+        session_start();
+
+        // Comprobacción del login
+        require_once('php/con-mysql/login_check.php');
+
+        // Imprimiendo pagina
+        echo $this->$paginaUp;
+        // Notificación errores
+        // Login, resultados o vacio
+        echo $this->$paginaDown;
+    }
+
+    function addContenido($contenido,$pos=0){
+        if($pos==1){
+            $this->paginaUp.= $contenido."\n";
+        }else{
+            $this->paginaDown.= $contenido."\n";
+        }
+    }
+
+    function cabecera(){
+        $usuario = empty($_SESSION['usuario']) && empty($_SESSION['password']) ? 'Invitado' : $_SESSION['usuario'];
+        $inicio = preg_match('/^index.php*/i',basename($_SERVER['REQUEST_URI'])) || preg_match('/proyectoPhp*/',basename($_SERVER['REQUEST_URI'])) ? 'active' : '';
+        $addAlumno = preg_match('/^insert_alumnos.php*/i',basename($_SERVER['REQUEST_URI'])) ? 'active' : '';
+        $editAlumno = preg_match('/^update_alumnos.php*/i',basename($_SERVER['REQUEST_URI'])) ? 'active' : '';
+        $delAlumno = preg_match('/^delete_alumnos.php*/i',basename($_SERVER['REQUEST_URI'])) ? 'active' : '';
+        $consultarAlumno = preg_match('/^select_alumnos.php*/i',basename($_SERVER['REQUEST_URI'])) ? 'active' : '';
+
+        $temp ="
+        <!DOCTYPE html>
+        <html lang='es'>
+        
+        <head>
+            <meta charset='UTF-8'>
+            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <meta name='viewport' content='initial-scale=1' />
+            <link rel='shortcut icon' href='images/favicon.png'>
+            <link rel='stylesheet' type='text/css' href='css/styles.css' />
+            <link rel='stylesheet' type='text/css' href='libs/bootstrap/css/bootstrap.min.css' />
+            <title>{$this->titulo}</title>
+        </head>
+        
+        <body>
+            <!--INICO CABECERA-->
+            <header id='header' class='sticky-top'>
+                <nav class='navbar navbar-expand-lg navbar-light navbar-dark bg-success sticky-top'>
+                    <div class='container-md'>
+                        <a class='navbar-brand logo' href='index.php?pagina=1'><img src='images/logo.png' />NombreEscuela</a>
+                        <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbar'
+                            aria-controls='navbar' aria-expanded='false' aria-label='Toggle navigation'>
+                            <span class='navbar-toggler-icon'></span>
+                        </button>
+                        <div class='collapse navbar-collapse' id='navbar'>
+                            <ul class='navbar-nav me-auto my-2 my-lg-0'>
+                                <li class='nav-item'>
+                                    <a class='nav-link {$inicio}' aria-current='page' href='index.php?pagina=1'>Inicio</a>
+                                </li>
+                                <li class='nav-item'>
+                                    <a class='nav-link active {$addAlumno}' href='insert_alumnos.php'>Añadir alumno</a>
+                                </li>
+                                <li class='nav-item'>
+                                    <a class='nav-link active {$editAlumno}' href='update_alumnos.php' tabindex='-1' aria-disabled='true'>Editar alumno</a>
+                                </li>
+                                <li class='nav-item'>
+                                    <a class='nav-link {$delAlumno}' href='delete_alumnos.php' tabindex='-1' aria-disabled='true'>Eliminar alumnos</a>
+                                </li>
+                                <li class='nav-item'>
+                                    <a class='nav-link {$consultarAlumno}' href='select_alumnos.php' tabindex='-1' aria-disabled='true'>Consultar alumnos</a>
+                                </li>
+                            </ul>
+                            <ul class='navbar-nav'>
+                                <li class='nav-item dropdown'>
+                                    <a class='nav-link dropdown-toggle active' id='navbarDropdown' role='button'
+                                        data-bs-toggle='dropdown' aria-expanded='false'>
+                                        {$usuario}
+                                    </a>
+                                    <ul class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                                        <li><a class='dropdown-item' href='login.php'>Iniciar sesión</a></li>
+                                        <li><a class='dropdown-item' href='register.php'>Registrarse</a></li>
+                                        <li>
+                                            <hr class='dropdown-divider'>
+                                        </li>
+                                        <li><a class='dropdown-item' href='php/logout.php'>Cerrar sesion</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <!-- FIN CABECERA -->
+        
+            <!-- Inicio de contenido -->
+            <main id='main' class='container container-md bg-light mt-2 p-5'>
+        ";
+
+        $this->addContenido($temp,0);
+    }
+
+    function footer(){
+        $temp = "
+            </main>
+            <!-- Fin de contenido -->
+    
+            <!--FOOTER-->
+            <footer if='footer' class='container-md bg-light mt-2 p-5'>
+                &copy; Created by <a href='https://www.pcmant.com/' target='_blank'>Juan Molina - PcMant</a>
+            </footer>
+    
+            <!--Scripts para bootstrap y dependencias-->
+            <script type='text/javascript' src='libs/bootstrap/js/bootstrap.bundle.min.js'></script>
+        </body>
+    
+        </html>
+        ";
+        $this->addContenido($temp,1);
+    }
+
+    function titulo($titulo,$h=0,$pos=0){
+        $temp = "<h{$h}>{$titulo}</h{$h}>";
+        $this->addContenido($temp,$pos);
+    }
+
+    function etiquetaGenerica($etiqueta,$contenido,$pos){
+        $temp = "<{$etiqueta}>{$contenido}</{$etiqueta}>";
+        $this->addContenido($temp,$pos);
+    }
+
+    function openForm(){}
+
+    function closeForm($pos=0){
+        $temp = '</form>';
+        $this->addContenido($temp,$pos);
+    }
+
+    function input(){}
+}
